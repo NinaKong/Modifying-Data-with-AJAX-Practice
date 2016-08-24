57ba64bb029d1bf899e3f53dfd3f31640207a021
NinaSleephead <kx1214@gmail.com>
2016-08-23 21:36:42 -0400

    Modifying-Data-with-AJAX-Practice
    Practice for modifying data with AJAX

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+var httpRequest; 
+if(window.XMLHttpRequest) {
+	httpRequest = new XMLHttpRequest();
+} else {
+	httpRequest = new ActiveXObject('Microsoft.XMLHTTP');
+}
+
+httpRequest.open('GET', 'data/data.txt');
+
+httpRequest.onreadystatechange = function(){
+	if((httpRequest.status == 200)&&(httpRequest.readyState==4)) {
+	    var data = document.getElementById('readData');
+	    data.innerHTML = httpRequest.responseText;
+
+	    var tagData = document.getElementsByTagName('li');
+	    for (var i = 0; i < tagData.length; i++) {
+	        tagData[i].innerHTML = httpRequest.responseText;
+	    }
+	}
+}
+httpRequest.send();
+
+
